c927fefe54524d7cb1c94bedc1b618b67b9df902
Parker Hicks <parkerhicks99@gmail.com>
2024-05-08 05:09:42 -0400

    changed to Sequential and added predict functionality

--- SNNomics/model.py
+++ SNNomics/model.py
@@ -4,23 +4,33 @@ import torch.nn.functional as F
 
 
 class SNN(nn.Module):
-    def __init__(self, _in: int):
+    def __init__(self, _in: int, task: str):
         super(SNN, self).__init__()
         self._in = _in
+        self.task = task
     
-        self.l0 = nn.Linear(self._in, 1000)
-        self.l1 = nn.Linear(1000, 500)
-        self.l2 = nn.Linear(500, 128)
+        self.fc = nn.Sequential(
+            nn.Linear(self._in, 1000),
+            nn.ReLU(inplace=True),
+            nn.Linear(1000, 128),
+        )
 
     def forward_once(self, x):
-        l0_out = F.relu(self.l0(x), inplace=True)
-        l1_out = F.relu(self.l1(l0_out), inplace=True)
-        l2_out = self.l2(l1_out)
-
-        return l2_out
-
-    def forward(self, input1, input2):
-        out1 = self.forward_once(input1)
-        out2 = self.forward_once(input2)
+        output = self.fc(x)
         
-        return out1, out2
+        return output
+
+    def forward(self, input1, input2, input3=None):
+        if self.task == 'train':
+            out1 = self.forward_once(input1)
+            out2 = self.forward_once(input2)
+            out3 = self.forward_once(input3)
+
+            return out1, out2, out3
+        
+        elif self.task == 'predict':
+            out1 = self.forward_once(input1)
+            out2 = self.forward_once(input2)
+            
+            return out1, out2
+
